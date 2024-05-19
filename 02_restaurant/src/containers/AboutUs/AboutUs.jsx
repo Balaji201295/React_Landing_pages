b77a0e089cc8cd5b6bdb25e1ff83b8c9423277df
fullstackdev <rajenbalaji12@gmail.com>
2024-05-19 14:12:34 +0530

    Navbar, Header, AboutUs, SpecialMenu, Chef(Video) sections completed

--- 02_restaurant/src/containers/AboutUs/AboutUs.jsx
+++ 02_restaurant/src/containers/AboutUs/AboutUs.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
+  <article
+    className="app__aboutUs app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutUs-overlay flex__center">
+      <img src={images.G} alt="G" />
+    </div>
+    <div className="app__aboutUs-content flex__center">
+      <div className="app__aboutUs-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="Spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+      <div className="app__aboutUs-content_knife flex__center">
+        <img src={images.knife} alt="Knife" />
+      </div>
+      <div className="app__aboutUs-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="Spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </article>
 );
 
 export default AboutUs;
